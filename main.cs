3b5ad742ec7129f695c651bbd43a9d86f517e41d
XiomarRob <157074573+XiomarRob@users.noreply.github.com>
2024-11-21 22:26:19 +0000

    added 0 to 30 condition

--- main.cs
+++ main.cs
@@ -56,11 +56,21 @@ internal class Program
         }
         return result;
     }
+    private static int GetInput2(string prompt)
+    {
+        int result;
+        Console.Write(prompt);
+        while  (!int.TryParse(Console.ReadLine(), out result) || result < 0 || result > 30)
+      {
+        Console.Write("Invalid input. Enter a number between 0 and 30: ");
+      }
+      return result;
+    }
     private static void CalculateRevenue()
     {
         Console.WriteLine("We will calculate the revenue for this year.");
-        int lastYearContestants = GetInput("Enter last year's number of contestants: ");
-        int thisYearContestants = GetInput("Enter this year's number of contestants: "); 
+        int lastYearContestants = GetInput("Enter last year's number of contestants (0-30): ");
+        int thisYearContestants = GetInput("Enter this year's number of contestants (0-30): "); 
 
         int ticketPrice = 25;
         int revenue = thisYearContestants * ticketPrice;
